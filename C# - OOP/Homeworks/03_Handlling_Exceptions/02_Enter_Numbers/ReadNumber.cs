f36f10d7e4954b8908d733eb190e899990e241aa
Borislav Hristov <borislav.irdsp@gmail.com>
2014-09-22 18:33:54 +0300

    Homeworks/03_Handlling_Exceptions/

---
+++ C# - OOP/Homeworks/03_Handlling_Exceptions/02_Enter_Numbers/ReadNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class ReadNumber
+{
+    //define a stitic method ReadNumber()
+    public static void ReadNumbers(int start, int end)
+    {
+        Console.WriteLine("Plese enter a number:");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number < start || number > end)
+        {
+            throw new ArgumentOutOfRangeException(number.ToString(),
+                String.Format("Number should be in the range [{0}..{1}]", start, end));
+        }
+    }
+
+    static void Main()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            try
+            {
+                ReadNumbers(1, 100);
+            }
+            catch (FormatException)
+            {
+                Console.Error.WriteLine("Invalid Number");
+                Console.WriteLine("Try again:");
+                i--;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.Error.WriteLine("Number should be in the range [1..100]");
+                Console.WriteLine("Try again:");
+                i--;
+            }
+        }
+    }
+}
