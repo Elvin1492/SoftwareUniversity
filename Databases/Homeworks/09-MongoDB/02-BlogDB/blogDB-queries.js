4574d9efa64aefe7aab3b8c7902669c4048f0e9d
borislavml <borislav.irdsp@gmail.com>
2015-02-24 22:22:35 +0200

    updated queries

--- Databases/Homeworks/09-MongoDB/02-BlogDB/blogDB-queries.js
+++ Databases/Homeworks/09-MongoDB/02-BlogDB/blogDB-queries.js
@@ -6,11 +6,9 @@ db.posts.aggregate([{
         _id: 0,
         "author.name": 1,
         title: 1,
-        category: 1,
         tags: 1
     }
 }]);
-
 
 // get all posts from category database
 db.posts.find({
@@ -22,3 +20,40 @@ db.posts.find({
     title: 1,
     "author.twitter": 1,
 });
+
+// get all posts from 2011
+db.posts.find({
+    date: {
+        "$gte": ISODate("2011-01-01T00:00:00Z"),
+        "$lt": ISODate("2012-01-01T00:00:00Z")
+    }
+}, {
+    _id: 0,
+    title: 1,
+    "author.linkedIn": 1,
+});
+
+// update  minka's tag Bentley to Bentley GT
+db.posts.update({
+    "author.name": {
+        $eq: "Minka Mazgaldjieva"
+    },
+    tags: {
+        $eq: "Bentley"
+    }
+}, {
+    $set: {
+        "tags.$": "Bentley GT"
+    }
+});
+
+
+// group by category and get count for each
+db.posts.aggregate([{
+    $group: {
+        _id: "$category",
+        posts: {
+            $sum: 1
+        }
+    }
+}]);
